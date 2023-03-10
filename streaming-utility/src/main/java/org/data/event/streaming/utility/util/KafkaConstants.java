532ab4a9dbd94937ffdb8a2a404245672f83ff1d
Jasmira Richelle Fernandes <jasmira@users.noreply.github.com>
2023-03-10 15:32:15 +0000

    Add files via upload

---
+++ streaming-utility/src/main/java/org/data/event/streaming/utility/util/KafkaConstants.java
@@ -0,0 +1,12 @@
+package org.data.event.streaming.utility.util;
+
+public class KafkaConstants {
+
+    public static final String BOOTSTRAP_SERVERS = "10.58.12.107:9092,10.58.12.107:9093,10.58.12.107:9094";
+    public static final String TOPIC = "test-topic";
+    public static final String GROUP_ID = "event-stream-consumers";
+    public static final String COMPRESSION_TYPE = "lz4";
+    public static final String TRUE = "true";
+    public static final String DATE_FORMAT = "MM/dd/yyyy hh:mm:ss";
+    public static final String ACCOUNT_NUMBER = "110011";
+}
